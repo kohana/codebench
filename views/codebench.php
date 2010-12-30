17bcd5411a7b060bcf8bfb760583203980876c2d
Bob Eagan <bob@synapsestudios.com>
2010-12-30 10:45:12 -0800

    fixing coding standards violation: Single-line if statements should only be used when breaking normal execution

--- views/codebench.php
+++ views/codebench.php
@@ -14,7 +14,9 @@
 <head>
 
 	<meta charset="utf-8" />
-	<title><?php if ($class !== '') echo $class, ' · ' ?>Codebench</title>
+	<title><?php if ($class !== ''): ?>
+			<?php echo $class, ' · ' ?>
+		<?php endif; ?>Codebench</title>
 
 	<style>
 		/* General styles*/
