eaeaed938c5243e190b34cb27d74a11113f25bba
Kiall Mac Innes <kiall@managedit.ie>
2011-09-23 17:20:53 +0100

    Fixing CS Issues. Uppercase Constants and Logical Operators

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 // Catch-all route for Codebench classes to run
 Route::set('codebench', 'codebench(/<class>)')
